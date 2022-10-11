70cb5a3d398793f54448a1d7d9b193f1bc01648c
AyoubVD <ayoub.disneyti@gmail.com>
2022-10-11 12:46:31 +0200

    captcha and register
    captcha wilt niet werken
    zoek op recaptcha

--- Scripts/login.js
+++ Scripts/login.js
@@ -17,4 +17,5 @@ function validation()
         return false;  
         }  
     }                             
-}  
+} 
+ 
--- test.php
+++ test.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
-    <title>Register</title>
+    <script src="https://www.google.com/recaptcha/enterprise.js?render=6Lfi3WkiAAAAAJ6gOLZWHGiwhnD6Whz8UAamD1oT"></script>
+
+    <title>test page</title>
 </head>
 <body>
 <main>
+    
     <form action="register.php" method="post">
         <h1>Sign Up</h1>
         <div>
@@ -23,6 +26,7 @@
             <input type="password" name="password" id="password">
         </div>
         <div>
+            
             <label for="password2">Password Again:</label>
             <input type="password" name="password2" id="password2">
         </div>
@@ -31,10 +35,19 @@
                 <input type="checkbox" name="agree" id="agree" value="yes"/> I agree
                 with the
                 <a href="#" title="term of services">term of services</a>
+                
             </label>
+            
+        </div>
+        <div>
+        
+        <input type="submit" />
         </div>
         <button type="submit">Register</button>
-        <footer>Already a member? <a href="login.php">Login here</a></footer>
+        <footer>Already a member? <a href="index.html">Login here</a></footer>
+        <?php
+                    include("captcha.php")
+                ?>
     </form>
 </main>
 </body>
