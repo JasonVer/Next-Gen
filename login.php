64d5ea28938bcea3f071f488b2d2cfa67b10f8e2
Frederic <fredericduwe1997@gmail.com>
2022-10-18 21:57:20 +0200

    redirect invalid user

--- login.php
+++ login.php
@@ -8,3 +8,7 @@ $pw_check = $database->query(("SELECT * FROM users WHERE username='".$username."
 $result = $pw_check->fetch_assoc();
 var_dump($result);
 //$count_check = $database->query("SELECT COUNT(*) AS c FROM users WHERE username ='" . $username ."' OR mail='".$email."'");
+
+if(!$result){
+    header('Location: ./index.html');
+}
