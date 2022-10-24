dc05a24904acc32db3aeb28841603d9b1882e127
Christophe <christophe.labeeu@icloud.com>
2022-10-24 21:50:31 +0200

    lesgo

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-$database = new mysqli("127.0.0.1:3306", "root","", "iamsocial");
+$database = new mysqli("127.0.0.1:3306", "root","", "suckit");
 if(!$database){
     die("there was an issue connecting to the database!");
 }
