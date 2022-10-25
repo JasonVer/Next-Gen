e6df9bbe59870b6656c285972f2cfcba74c6b36e
frederic2707 <fredericduwe1997@gmail.com>
2022-10-25 14:40:08 +0200

    poging om db te connecten op combell

--- database.php
+++ database.php
@@ -1,5 +1,6 @@
 <?php
-$database = new mysqli("127.0.0.1:3306", "root","", "iamsocial");
+//$database = new mysqli("127.0.0.1:3306", "root","", "iamsocial");
+$database = new mysqli("ID388827_iamsocial.db.webhosting.be", "ID388827_iamsocial","wachtwoordimslab2020", "ID388827_iamsocial");
 if(!$database){
     die("there was an issue connecting to the database!");
 }
